7544698e61d5181419e1c47c993bd320f3afcb31
freezecyber <37415540+freezecyber@users.noreply.github.com>
2018-06-11 17:16:57 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace argsconsol
+{
+    class Program
+    {
+        private static ConsoleKeyInfo hdf;
+        private static ProcessStartInfo kajs;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string ffl = "c:/bin/spkconsol.exe";
+            Console.WriteLine("openfiledialog  : y :");
+            hdf = Console.ReadKey();
+            if (hdf.Key == ConsoleKey.Y)
+            {
+                OpenFileDialog ajd = new OpenFileDialog();
+                DialogResult lak = new DialogResult();
+
+                lak = ajd.ShowDialog();
+
+                if (lak == DialogResult.OK)
+                {
+                    ffl = ajd.FileName;
+
+                }
+            }else
+            {
+                Console.WriteLine("ENTER PATH FILENAME OR EMPTY TO PLAINCONSOL---->>");
+
+                var shd = Console.ReadLine();
+
+                if(shd != string.Empty)
+                {
+                    ffl = shd.ToString();
+                    Console.WriteLine("found");
+
+                }
+                else
+                {
+                    Console.WriteLine("spk consol");
+                }
+
+            }
+              
+
+                Console.WriteLine("ENTER ARGUMENT PROSSINFO");
+
+
+                var sgd = Console.ReadLine();
+
+
+            if (sgd != string.Empty)
+            {
+                kajs = new ProcessStartInfo(ffl);
+                kajs.Arguments = sgd;
+            }
+            else
+            {
+                kajs = new ProcessStartInfo(ffl);
+            }
+            Process pklo =  Process.Start(kajs);
+            pklo.WaitForExit();
+            Console.WriteLine("wait to exit : "+ffl);
+
+           
+
+
+         
+         
+
+
+        }
+    }
+}
